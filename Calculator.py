b07dfda6c215873f54bddbc6b6bf1a1f4d8c62fd
ruriru <56721970+ruriru@users.noreply.github.com>
2020-12-06 15:26:24 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,41 +1,43 @@
 from art import logo
 
-print(logo)
-
 def add(n1, n2):
   return n1 + n2
 
-
-def substract(n1, n2):
+def subtract(n1, n2):
   return n1 - n2
-
 
 def multiply(n1, n2):
   return n1 * n2
 
-
 def divide(n1, n2):
   return n1 / n2
 
-
-operations ={
+operations = {
   "+": add,
-  "-": substract,
+  "-": subtract,
   "*": multiply,
   "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+  print(logo)
 
-for symbol in operations:
-  print(symbol)
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input("Pick an operation symbol from the line above: ")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
 
-num2 = int(input("What's the second number?: "))
-
-calculation_function = operations[operation_symbol]
-
-answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
